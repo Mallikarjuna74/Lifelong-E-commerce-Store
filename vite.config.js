cc39643fcd0d289e4fba03635e11c4eb68be2572
Mallikarjuna74 <164886670+Mallikarjuna74@users.noreply.github.com>
2025-12-04 13:50:29 +0530

    Fixed errror in Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/lifelong-website/',
-  server: {
-    middlewareMode: true,
-  },
 })
