02a6c019b032e8a2b33664ab7bfd963fa89ef5e4
Mallikarjuna74 <164886670+Mallikarjuna74@users.noreply.github.com>
2026-02-03 11:55:50 +0530

    Updated url name in router

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const PublicRoute = ({ children }) => {
 
 // This component contains the main layout and routing logic
 const AppRouter = () => (
-  <Router basename="/lifelong-website">
+  <Router basename="/Lifelong-E-commerce-Store/">
     <Header />
     <BottomHeader />
     <div className="page-content-wrapper">
